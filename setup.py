6547f5de45f01c82edc04f65f7087430cdef71e0
Parantez Adam <xoamfatih96@gmail.com>
2021-01-11 21:48:39 +0300

    nvm lmfao

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="passwordtools-TriC",
+    name="passwordtools",
     version="0.0.1",
     author="TriC",
     author_email="parantezdev@gmail.com",
