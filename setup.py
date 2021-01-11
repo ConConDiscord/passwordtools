dd13ae0fcff621a513871c9dbf74bb333b43effc
Parantez Adam <xoamfatih96@gmail.com>
2021-01-11 21:58:50 +0300

    Yesn't.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="passwordtools",
-    version="0.0.1",
+    version="0.0.2",
     author="TriC",
     author_email="parantezdev@gmail.com",
     description='Fast password module using the "io" and "random" modules.',
